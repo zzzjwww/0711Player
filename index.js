11576288be982d1dc1c90f71ea48251b237af4af
Zijian Wang <zw@zijiandembp.myfiosgateway.com>
2021-07-11 21:56:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+console.log(video._prototype);
+
+function togglePlay(){
+ const method = video.paused ? "play" : "pause";
+ video[method]();
+}
+function updateButton(){
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+function skip() {
+  console.log(typeof(this.dataset.skip));
+  video.currentTime += Number(this.dataset.skip);
+
+}
+
+video.addEventListener("click",togglePlay);
+video.addEventListener("play",updateButton);
+video.addEventListener("pause",updateButton);
+
+toggle.addEventListener("click",togglePlay);
+skipButtons.forEach(element => {
+  element.addEventListener("click",skip)
+  
+});
