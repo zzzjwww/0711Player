44b3e1d929cc1183427dab69e1ba80f542f73955
Zijian Wang <zw@zijiandembp.myfiosgateway.com>
2021-07-12 09:14:59 -0400

    Added bar controls

--- index.js
+++ index.js
@@ -5,28 +5,45 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
-console.log(video._prototype);
+
 
 function togglePlay(){
  const method = video.paused ? "play" : "pause";
  video[method]();
 }
+
 function updateButton(){
   const icon = this.paused ? "►" : "❚ ❚";
   toggle.textContent = icon;
 }
-function skip() {
-  console.log(typeof(this.dataset.skip));
-  video.currentTime += Number(this.dataset.skip);
 
+function skip() {
+  video.currentTime += Number(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+video[this.name] = this.value;
+}
+
+function handleProgress(){
+  const percentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percentage}%`;
+
+}
+
+function scrub(e){
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
 
 video.addEventListener("click",togglePlay);
 video.addEventListener("play",updateButton);
 video.addEventListener("pause",updateButton);
-
+video.addEventListener("timeupdate",handleProgress);
 toggle.addEventListener("click",togglePlay);
 skipButtons.forEach(element => {
-  element.addEventListener("click",skip)
-  
+  element.addEventListener("click",skip)  
 });
+ranges.forEach(range =>range.addEventListener("change",handleRangeUpdate));
+ranges.forEach(range =>range.addEventListener("mousemove",handleRangeUpdate));
+progress.addEventListener("click",scrub);
